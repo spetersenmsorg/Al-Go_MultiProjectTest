9a0ffa0d485df5eb799f2f8141ac4d2df017fa87
spetersenms <spetersenms@users.noreply.github.com>
2025-07-10 11:39:38 +0000

    [main@56f32d7] Update AL-Go System Files from spetersenms/AL-Go@ExtendedDebugLogging - 6c8aaa5

--- Project1/.AL-Go/cloudDevEnv.ps1
+++ Project1/.AL-Go/cloudDevEnv.ps1
@@ -51,11 +51,11 @@ Write-Host -ForegroundColor Yellow @'
 
 $tmpFolder = Join-Path ([System.IO.Path]::GetTempPath()) "$([Guid]::NewGuid().ToString())"
 New-Item -Path $tmpFolder -ItemType Directory -Force | Out-Null
-$GitHubHelperPath = DownloadHelperFile -url 'https://raw.githubusercontent.com/spetersenms/AL-Go/DeployToEnvVar/Actions/Github-Helper.psm1' -folder $tmpFolder -notifyAuthenticatedAttempt
-$ReadSettingsModule = DownloadHelperFile -url 'https://raw.githubusercontent.com/spetersenms/AL-Go/DeployToEnvVar/Actions/.Modules/ReadSettings.psm1' -folder $tmpFolder
-$ALGoHelperPath = DownloadHelperFile -url 'https://raw.githubusercontent.com/spetersenms/AL-Go/DeployToEnvVar/Actions/AL-Go-Helper.ps1' -folder $tmpFolder
-DownloadHelperFile -url 'https://raw.githubusercontent.com/spetersenms/AL-Go/DeployToEnvVar/Actions/settings.schema.json' -folder $tmpFolder | Out-Null
-DownloadHelperFile -url 'https://raw.githubusercontent.com/spetersenms/AL-Go/DeployToEnvVar/Actions/Packages.json' -folder $tmpFolder | Out-Null
+$GitHubHelperPath = DownloadHelperFile -url 'https://raw.githubusercontent.com/spetersenms/AL-Go/ExtendedDebugLogging/Actions/Github-Helper.psm1' -folder $tmpFolder -notifyAuthenticatedAttempt
+$ReadSettingsModule = DownloadHelperFile -url 'https://raw.githubusercontent.com/spetersenms/AL-Go/ExtendedDebugLogging/Actions/.Modules/ReadSettings.psm1' -folder $tmpFolder
+$ALGoHelperPath = DownloadHelperFile -url 'https://raw.githubusercontent.com/spetersenms/AL-Go/ExtendedDebugLogging/Actions/AL-Go-Helper.ps1' -folder $tmpFolder
+DownloadHelperFile -url 'https://raw.githubusercontent.com/spetersenms/AL-Go/ExtendedDebugLogging/Actions/settings.schema.json' -folder $tmpFolder | Out-Null
+DownloadHelperFile -url 'https://raw.githubusercontent.com/spetersenms/AL-Go/ExtendedDebugLogging/Actions/Packages.json' -folder $tmpFolder | Out-Null
 
 Import-Module $GitHubHelperPath
 Import-Module $ReadSettingsModule
